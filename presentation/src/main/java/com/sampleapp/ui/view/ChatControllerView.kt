c0b774683c3e7da71e8965927d8ed83ca2b4ef0d
Anton Khorunzhiy <an.khorunzhiy@gmail.com>
2017-07-07 17:19:02 +0300

    added calendar

--- presentation/src/main/java/com/sampleapp/ui/view/ChatControllerView.kt
+++ presentation/src/main/java/com/sampleapp/ui/view/ChatControllerView.kt
@@ -8,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import com.android.newssample.R
+import com.sampleapp.domain.model.Event
 import com.sampleapp.domain.model.EventModel
+import kotlinx.android.synthetic.main.item_message.view.*
+import kotlinx.android.synthetic.main.item_user.view.*
 import kotlinx.android.synthetic.main.view_chat.view.*
 
 
@@ -40,20 +43,26 @@ class ChatControllerView @JvmOverloads constructor(
         override fun getItemCount(): Int = events.size
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+            (holder as BaseHolder).bind(events[position])
         }
 
         override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
             val inflater = LayoutInflater.from(context)
             when(viewType){
                 Type.MESSAGE.ordinal -> return MessageHolder(inflater.inflate(R.layout.item_message, null))
-                Type.USER_STATUS.ordinal -> return UserStatusHolder(inflater.inflate(R.layout.item_message, null))
-                Type.TYPING.ordinal -> return TypingHolder(inflater.inflate(R.layout.item_message, null))
+                Type.USER_STATUS.ordinal -> return UserStatusHolder(inflater.inflate(R.layout.item_user, null))
+                Type.TYPING.ordinal -> return TypingHolder(inflater.inflate(R.layout.item_user, null))
             }
             throw IllegalArgumentException("Unknown view type")
         }
 
         override fun getItemViewType(position: Int): Int {
-            return events[position].event.ordinal
+            when(events[position].event){
+                Event.NEW_MESSAGE -> return Type.MESSAGE.ordinal
+                Event.USER_JOINED, Event.USER_LEFT -> Type.USER_STATUS
+                Event.TYPING, Event.STOP_TYPING -> Type.TYPING
+            }
+            throw IllegalArgumentException("Unknown type")
         }
 
         override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
@@ -61,28 +70,36 @@ class ChatControllerView @JvmOverloads constructor(
             recyclerView?.let { this.context = context }
         }
 
-        class MessageHolder(view: View) : RecyclerView.ViewHolder(view){
+        class MessageHolder(view: View) : BaseHolder(view){
 
-            fun bind(eventModel: EventModel){
-
+            override fun bind(eventModel: EventModel){
+                itemView.user_name.text = eventModel.userName
+                itemView.message.text = eventModel.message
             }
 
         }
 
-        class TypingHolder(view: View): RecyclerView.ViewHolder(view){
+        class TypingHolder(view: View): BaseHolder(view){
 
-            fun bind(eventModel: EventModel){
-
+            override fun bind(eventModel: EventModel){
+                val typingText = itemView.resources.getString(R.string.is_typing, eventModel.userName)
+                itemView.user.text = typingText
             }
 
         }
 
-        class UserStatusHolder(view: View): RecyclerView.ViewHolder(view){
+        class UserStatusHolder(view: View): BaseHolder(view){
 
-            fun bind(eventModel: EventModel){
-
+            override fun bind(eventModel: EventModel){
+                val event = eventModel.event
+                val stringId = if(event == Event.USER_JOINED) R.string.has_joined else R.string.has_left
+                itemView.user.text = itemView.resources.getText(stringId, eventModel.userName)
             }
 
+        }
+
+        abstract class BaseHolder(view: View) : RecyclerView.ViewHolder(view){
+            abstract fun bind(eventModel: EventModel)
         }
     }
 }
