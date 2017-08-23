800c41f14bf877505dab8727e9a4db2b6194a98f
Anton Khorunzhiy <an.khorunzhiy@gmail.com>
2017-08-23 11:09:09 +0300

    fix lint issues

--- presentation/src/main/java/com/sampleapp/MainActivity.kt
+++ presentation/src/main/java/com/sampleapp/MainActivity.kt
@@ -1,25 +1,17 @@
 package com.sampleapp
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
-import android.view.Menu
-import android.view.MenuItem
 import com.android.newssample.R
 import com.bluelinelabs.conductor.Conductor
 import com.sampleapp.navigation.ControllerMediator
 import com.sampleapp.ui.controller.Layout
 import com.sampleapp.ui.controller.LoginController
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import javax.inject.Inject
 
-@Layout(R.layout.activity_main)
-class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+@Layout(R.layout.app_bar_main)
+class MainActivity : BaseActivity() {
 
     @Inject
     lateinit var mediator: ControllerMediator
@@ -37,28 +29,12 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
     private fun prepareUI() {
         setSupportActionBar(toolbar)
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        val navigationView = findViewById(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else if (!router.handleBack()) {
+        if (!router.handleBack()) {
             super.onBackPressed()
         }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        return true
     }
 
 }
